f57f57126ff172f1a96d8997bc5f088a719f154e
Amarokotlandgit <carlostapia567@gmail.com>
2024-08-17 22:06:16 -0400

    agregue el prueba1.py

---
+++ prueba1.py
@@ -0,0 +1,8 @@
+# Sorprende a tus compañeros
+import random
+platos = ["lazaña", "hamburguesa","pizza"]
+print(platos[0])
+print(platos[1])
+print(platos[2])
+print ("eleccion aleatoria:")
+print(random.choice(platos))
